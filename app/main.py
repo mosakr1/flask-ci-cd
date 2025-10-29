1cded46831b25a78971d6494b9c58a5ae0f66da7
mosakr1 <sakr2137@gmail.com>
2025-10-29 19:57:33 +0300

    Trigger CI/CD pipeline

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Hello, DevOps Engineer☻! Current time: {datetime.now()}"
+    return f"Hello, DevOps Engineer☻.. Current time: {datetime.now()}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5005)
