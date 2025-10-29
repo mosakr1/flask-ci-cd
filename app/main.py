054fcb7081edebc0fabf9b0cc9efae48c409e390
mosakr1 <sakr2137@gmail.com>
2025-10-29 19:39:05 +0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return f"Hello, DevOps Engineer! Current time: {datetime.now()}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5005)
